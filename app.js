6792d7dc0cb761c7941dad05c27a59a0fe603818
Barkod1 <80260333+Barkod1@users.noreply.github.com>
2021-05-12 22:40:29 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// let names = 0;
+// let ids = 0;
+// function add(id) {
+//     let newIn = document.createElement("input");
+//     let addafter = document.querySelector(".add");
+
+//     addafter.insertAdjacentElement("afterend", newIn);
+//     // for (let i = 0; i < 3; i++) {
+//     //     let newIn = CreateElement("input");
+//     //     tds.appendChild(input);
+//     //     tds.appendChild(input);
+
+//     // }
+
+
+// }
