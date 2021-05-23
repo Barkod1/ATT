a0adc14fdf9ad64c18c066ce7d5d5d7fb386bb95
Barkod1 <80260333+Barkod1@users.noreply.github.com>
2021-05-23 21:49:03 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,66 +1,98 @@
-function addh() {
-    let inputs = document.querySelectorAll("input");
-
-}
-
-
 let addness = 0;
+naming();
+addEventListenerInput();
 
 function add() {
     let tfoot = document.querySelectorAll("tbody");
     for (let j = 0; j < 4; j++) {
-        tfoot[j].appendChild(createEl('tr'));
-        for (let i = 0; i < 2; i++) {
-            tfoot[j].lastChild.appendChild(createEl('td'));
-            tfoot[j].lastChild.children[i].appendChild(createEl('input'));
-            tfoot[j].lastChild.children[i].lastChild.name = `addness-${addness}`
-            addness++;
-        }
+        addInput(tfoot[j]);
+    }
+    for (let j = 4; j < 6; j++) {
+        addInputForData(tfoot[j]);
+    }
+    addInputForDataArr(tfoot[6]);
+}
 
-    }
-    for (let k = 4; k < 6; k++) {
-        tfoot[k].appendChild(createEl('tr'));
-        tfoot[k].lastChild.appendChild(createEl('td'));
-        tfoot[k].lastChild.children[0].appendChild(createEl('input'));
-        tfoot[k].lastChild.children[0].lastChild.name = `addness-${addness}`
-        addness++;
-    }
-    for (let x = 6; x < 16; x++) {
-        tfoot[x].appendChild(createEl('tr'));
-        for (let y = 0; y < 11; y++) {
-            tfoot[x].lastChild.appendChild(createEl('td'));
-            tfoot[x].lastChild.children[y].appendChild(createEl('input'));
-            tfoot[x].lastChild.children[y].lastChild.name = `addness-${addness}`
-            addness++;
-        }
-    }
+function addInput(tbody) {
+    let tr = createEl("tr");
+    let td1 = tr.appendChild(createEl('td'));
+    let td2 = tr.appendChild(createEl('td'));
+    let input1 = td1.appendChild(createEl('input'));
+    let input2 = td2.appendChild(createEl('input'));
+    input1.name = `addness-${addness}`;
+    addness++;
+    input2.name = `addness-${addness}`;
+    addness++;
+    addEventListenerInputSpec(input1);
+    addEventListenerInputSpec(input2);
+    input1.maxLength = 3;
+    input2.maxLength = 3;
+    tbody.appendChild(tr);
 
 }
 
+function addInputForData(tbody) {
+    let tr = createEl("tr");
+    let td1 = tr.appendChild(createEl('td'));
+    let input1 = td1.appendChild(createEl('input'));
+    input1.name = `addness-${addness}`;
+    addness++;
+    input1.maxLength = 3;
+    addEventListenerInputSpec(input1);
+    tbody.appendChild(tr);
+}
+
+function addInputForDataArr(tbody) {
+    let tr = createEl("tr");
+    for (let x = 6; x < 17; x++) {
+        let td = tr.appendChild(createEl('td'));
+        let input = td.appendChild(createEl('input'));
+        input.name = `addness-${addness}`;
+        addness++;
+        input.maxLength = 3;
+        addEventListenerInputSpec(input);
+    }
+    tbody.appendChild(tr);
+}
 
 function createEl(el) {
     let newEl = document.createElement(el);
     return newEl;
 }
 
-function findLastTr(table) {
-    let i = 0;
-    let tables = document.querySelectorAll("table")[table].children;
-
-    while (tables[tables.length - i].tagName != "tr") {
-        tables = document.querySelectorAll("table")[table].children;
-        tables = tables[tables.length - i];
-        i++;
-    }
-    return tr;
-}
 
 function d() {
-    for (let i = 0; i < 16; i++) {
-        let tbody = document.querySelectorAll("tbody")[i];
-        let tr = tbody.querySelectorAll("tr");
-
-        tbody.removeChild(tr[tr.length - 1]);
+    let tbodies = document.querySelectorAll("tbody");
+    for (let tbody of tbodies) {
+        tbody.lastElementChild.remove();
     }
+    addness -= 21;
+}
 
+function naming() {
+    let inputs = document.querySelectorAll('input');
+    for (input of inputs) {
+        input.name = `addness-${addness}`
+        input.maxLength = 3;
+        addness++;
+    }
+}
+
+function addEventListenerInput() {
+    let inputs = document.querySelectorAll('input');
+    for (let input of inputs) {
+        input.addEventListener('change', () => {
+            if (input.value[input.value.length - 1] != "h")
+                input.value = `${input.value}h`;
+
+        }
+        );
+    }
+}
+
+function addEventListenerInputSpec(input) {
+    input.addEventListener('change', () => {
+        input.value = `${input.value}h`;
+    }
+    )
 }
